4d028bf5a685fd31a20b1fe31f22e9cf4d4a5521
Tony Chow <totochow@hotmail.com>
2021-12-22 15:55:41 -0800

    Commit

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+                //"MoveiesByReleaseDate",
+                //"movies/released/{year}/{month}",
+                //new { controller = "Movies", action = "ByReleaseDate" },
+                //new { year = @"\d{4}", month = @"\d{2}" }
+                //new { year = @"2015|2016|2018|2019", month = @"\d{2}" }
+                //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //var ViewResult = new ViewResult();
+            //ViewResult.ViewData.Model
+
+            return View(viewModel);
+            //return Content("Hellow World!");
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+
+        }
+
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("id= " + Id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
